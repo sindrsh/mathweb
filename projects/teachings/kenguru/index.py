8ff57347a313b1523711db49ceb34a885b6772be
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2025-10-02 07:40:34 +0200

    p

---
+++ projects/teachings/kenguru/index.py
@@ -0,0 +1,3 @@
+title = "Kenguru"
+tags = ["logikk", "visualisering", "ligninger", "geometri", "multiplikasjon", "divisjon", "addisjon", "subtraksjon", "brøk"]
+grades = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1P", "2P", "1T", "R1", "R2"]
